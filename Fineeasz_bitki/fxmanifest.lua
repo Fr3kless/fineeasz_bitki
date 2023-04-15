6b7e7d10d73e0c8814b9343a5e71b1a6851ffa18
fr3klesior <73782648+Fr3kless@users.noreply.github.com>
2023-04-15 20:19:50 +0200

    Add files via upload

---
+++ Fineeasz_bitki/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Fineeasz'
+
+client_scripts {
+    'config.lua',
+    'client/spheres.lua',
+    'client/main.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'config.lua',
+    'server/main.lua'
+}
+
+shared_scripts { 
+    '@es_extended/imports.lua',
+}
